26bd665a12f5fc8509041760cf7469055ed4f27c
tonyjiangde <tao.jiang02@sap.com>
2021-11-09 15:33:23 +0800

    First commit

---
+++ core-customize/custom/hybrisShanghai/gensrc/org/hybris/shanghai/constants/GeneratedHybrisShanghaiConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Sep 14, 2021, 11:46:10 AM                   ---
+ * ----------------------------------------------------------------
+ */
+package org.hybris.shanghai.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedHybrisShanghaiConstants
+{
+	public static final String EXTENSIONNAME = "hybrisShanghai";
+	
+	protected GeneratedHybrisShanghaiConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
