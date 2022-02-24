8389725396e37397daac25b1b920f6be09a48cd7
sapsupporter <85480670+sapsupporter@users.noreply.github.com>
2022-02-24 17:25:56 +0800

    init

--- README.md
+++ README.md
@@ -1,24 +1,2 @@
-# commerce-cloud-ps-apj
-Repo for ps apj
-
-# Branch naming convention: <Commerce_Major_Version>+<additional_remark>
-# Sample manifest contains Commerce b2b, b2c, smartedit, mediaconversion, cloud hot folder, scpi and spartacus.
-# Commerce Version 
-1. 1811(EOMM)(CEP 1811)
-2. 1905(CEP1905)
-3. 2005(IntPack2005)
-4. 2011(IntPack2102)
-5. 2105 (IntPack2108)
-
-# How to use CEP (CEP is deprecated since 2005, use integration pack instead)
-"commerceSuiteVersion": "1905",
-"useCloudExtensionPack": true,
-
-# How to use integration pack(https://help.sap.com/viewer/2f43049ad8e443249e1981575adddb5d/2108/en-US/19bacaecbdd34cc8bd58bdd8daf428c5.html)
-"commerceSuiteVersion": "2005",    
-"extensionPacks": [
-        {
-            "name": "hybris-commerce-integrations",
-            "version": "2005.5"
-        }
-    ],
+# commerce-cloud-ps-apj-2011sample
+ 
---
+++ custom/training/trainingfulfilmentprocess/gensrc/de/hybris/training/fulfilmentprocess/constants/GeneratedTrainingFulfilmentProcessConstants.java
@@ -0,0 +1,35 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Feb 15, 2022, 6:46:31 PM                    ---
+ * ----------------------------------------------------------------
+ *  
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.training.fulfilmentprocess.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingFulfilmentProcessConstants
+{
+	public static final String EXTENSIONNAME = "trainingfulfilmentprocess";
+	public static class Attributes
+	{
+		public static class ConsignmentProcess
+		{
+			public static final String DONE = "done".intern();
+			public static final String WAITINGFORCONSIGNMENT = "waitingForConsignment".intern();
+			public static final String WAREHOUSECONSIGNMENTSTATE = "warehouseConsignmentState".intern();
+		}
+	}
+	
+	protected GeneratedTrainingFulfilmentProcessConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
